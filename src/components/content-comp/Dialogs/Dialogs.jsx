c0feca79e47c4332b0049f452f368ee45d9210bf
adminandrej <andrej.darmenko@gmail.com>
2024-05-22 15:34:58 +0300

    Dialog comp. Some verstka +

--- src/components/content-comp/Dialogs/Dialogs.jsx
+++ src/components/content-comp/Dialogs/Dialogs.jsx
@@ -4,8 +4,8 @@ import css from './Dialogs.module.css';
 const Dialogs = () => {
   return (
     <div className={css.dialogs}>
-      <div>
-        <h4>Dialogs</h4>
+      <div className={ css.allChats }>
+        <h4>Все чаты</h4>
 
         <div className="ul">
           <div className="li">
@@ -16,6 +16,7 @@ const Dialogs = () => {
             </a>
             <a href="">RT</a>
           </div>
+
           <div className="li">
             <a href=""><img 
               src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
@@ -24,6 +25,7 @@ const Dialogs = () => {
             </a>
             <a href="">Мир вокруг</a>
           </div>
+
           <div className="li">
             <a href=""><img 
               src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
@@ -32,6 +34,7 @@ const Dialogs = () => {
             </a>
             <a href="">Любимая</a>
           </div>
+
           <div className="li">
             <a href=""><img 
               src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
@@ -40,6 +43,7 @@ const Dialogs = () => {
             </a>
             <a href="">Доня</a>
           </div>
+
           <div className="li">
             <a href=""><img 
               src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
@@ -51,10 +55,51 @@ const Dialogs = () => {
         </div>
       </div>
 
-      <div>
-        <h4>Messages</h4>
+      <div className={ css.messages }>
+        <h4>Сообщения</h4>
 
+        <div className={ css.items }>
+          <div className={ css.item }>
+            <img src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
+              alt="" 
+              className={ css.avaImg } 
+            />
+            <div className={ css.messText }>Привет</div>
+          </div>
 
+          <div className={ css.item }>
+            <img src="https://avatars.mds.yandex.net/i?id=be935738007a47dbd50bbb658b223e462b45b692-10496841-images-thumbs&n=13" 
+              alt="" 
+              className={ css.avaImg } 
+            />
+            <div className={ css.messText }>Привет</div>
+          </div>
+
+          <div className={ css.item }>
+            <img src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
+              alt="" 
+              className={ css.avaImg } 
+            />
+            <div className={ css.messText }>Привет</div>
+          </div>
+
+          <div className={ css.item }>
+            <img src="https://avatars.mds.yandex.net/i?id=be935738007a47dbd50bbb658b223e462b45b692-10496841-images-thumbs&n=13" 
+              alt="" 
+              className={ css.avaImg } 
+            />
+            <div className={ css.messText }>Привет</div>
+          </div>
+
+          <div className={ css.item }>
+            <img src="https://avatars.mds.yandex.net/i?id=13b1234f4c0f1e5730c6c65904b04179-4233102-images-thumbs&n=13" 
+              alt="" 
+              className={ css.avaImg } 
+            />
+            <div className={ css.messText }>Привет</div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
